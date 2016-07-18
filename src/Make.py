c3174c62c42c6cff49a86089dc9ae707ffe39f05
Piotr Derkowski <piotder@gmail.com>
2016-07-18 16:38:17 +0200

    Added pagerank compilation to make

--- src/Make.py
+++ src/Make.py
@@ -9,6 +9,9 @@ import urllib
 paths = Paths(Utils.getParentDirectory(__file__))
 paths.include()
 
+def fileIsOlder(file, dependencies):
+    return all(os.path.exists(dep) and os.path.getctime(dep) < os.path.getctime(file) for dep in dependencies)
+
 # DIRS
 def createDirs():
     for d in [paths.binDir, paths.dataDir]:
@@ -26,12 +29,26 @@ def downloadData():
 def skipDownloadingData():
     return all(os.path.exists(d) for d in [paths.pageTable, paths.linksTable])
 
+# COMPILATION
+def compileSources():
+    logger = logging.getLogger(__name__)
+
+    command = "g++ -std=c++11 -o {} {}".format(paths.pagerankBin, ' '.join(paths.pagerankSources))
+    logger.info('Running command: {}'.format(command))
+
+    subprocess.call(command, shell=True)
+
+def skipCompilation():
+    return os.path.exists(paths.pagerankBin) and fileIsOlder(paths.pagerankBin, paths.pagerankSources)
+
+
 def main():
     Utils.configLogging()
 
     jobs = Jobs()
     jobs.add(Job('CREATE DIRECTORIES', createDirs, skipCreatingDirs))
     jobs.add(Job('DOWNLOAD DATA', downloadData, skipDownloadingData))
+    jobs.add(Job('COMPILE SOURCES', compileSources, skipCompilation))
     jobs.run()
 
 if __name__ == "__main__":
