5f37ea9b654badaa52e5317be1fcd7a131df7ae9
Piotr Derkowski <piotder@gmail.com>
2016-12-19 01:49:34 +0100

    Fixed app crashing when searching for words containing non-ascii letters.

--- SQLTableDefs.py
+++ SQLTableDefs.py
@@ -5,7 +5,7 @@ class HighDimensionalNeighborsTable(TableProxy):
         super(HighDimensionalNeighborsTable, self).__init__(tablePath, useCustomTypes=True)
 
     def create(self):
-        self.execute(Query("""
+        self.execute(Query(u"""
             CREATE TABLE hdnn (
                 hdnn_id                 INTEGER     NOT NULL    PRIMARY KEY,
                 hdnn_neighbors_ids      LIST        NOT NULL,
@@ -13,14 +13,14 @@ class HighDimensionalNeighborsTable(TableProxy):
             );"""))
 
     def populate(self, values):
-        self.executemany(Query("INSERT INTO hdnn VALUES (?,?,?)", "populating hdnn table", logStart=True), values)
+        self.executemany(Query(u"INSERT INTO hdnn VALUES (?,?,?)", "populating hdnn table", logStart=True), values)
 
 class LowDimensionalNeighborsTable(TableProxy):
     def __init__(self, tablePath):
         super(LowDimensionalNeighborsTable, self).__init__(tablePath, useCustomTypes=True)
 
     def create(self):
-        self.execute(Query("""
+        self.execute(Query(u"""
             CREATE TABLE ldnn (
                 ldnn_id                 INTEGER     NOT NULL    PRIMARY KEY,
                 ldnn_neighbors_ids      LIST        NOT NULL,
@@ -28,14 +28,14 @@ class LowDimensionalNeighborsTable(TableProxy):
             );"""))
 
     def populate(self, values):
-        self.executemany(Query("INSERT INTO ldnn VALUES (?,?,?)", "populating ldnn table", logStart=True), values)
+        self.executemany(Query(u"INSERT INTO ldnn VALUES (?,?,?)", "populating ldnn table", logStart=True), values)
 
 class WikimapPointsTable(TableProxy):
     def __init__(self, tablePath):
         super(WikimapPointsTable, self).__init__(tablePath, useCustomTypes=True)
 
     def create(self):
-        self.execute(Query("""
+        self.execute(Query(u"""
             CREATE TABLE wikipoints (
                 wp_id                   INTEGER     NOT NULL    PRIMARY KEY,
                 wp_title                TEXT        NOT NULL,
@@ -51,7 +51,7 @@ class WikimapPointsTable(TableProxy):
             );"""))
 
     def populate(self, values):
-        self.executemany(Query("""
+        self.executemany(Query(u"""
             INSERT INTO wikipoints(
                 wp_id,
                 wp_title,
@@ -64,10 +64,10 @@ class WikimapPointsTable(TableProxy):
             VALUES (?,?,?,?,?,?,?,?)
         """, "populating wikipoints table", logStart=True), values)
 
-        self.execute(Query("CREATE UNIQUE INDEX title_idx ON wikipoints(wp_title);", "creating index title_idx in wikipoints table", logStart=True, logProgress=True))
+        self.execute(Query(u"CREATE UNIQUE INDEX title_idx ON wikipoints(wp_title);", "creating index title_idx in wikipoints table", logStart=True, logProgress=True))
 
     def updateIndices(self, values):
-        self.executemany(Query("""
+        self.executemany(Query(u"""
             UPDATE wikipoints
             SET
                 wp_xindex = ?,
@@ -78,38 +78,38 @@ class WikimapPointsTable(TableProxy):
             """, "updating indices in wikipoints table", logStart=True), values)
 
     def selectCoordsAndIds(self):
-        return self.select(Query("SELECT wp_x, wp_y, wp_id FROM wikipoints"))
+        return self.select(Query(u"SELECT wp_x, wp_y, wp_id FROM wikipoints"))
 
     def selectIds(self):
-        return self.select(Query("SELECT wp_id FROM wikipoints"))
+        return self.select(Query(u"SELECT wp_id FROM wikipoints"))
 
     def selectByTitle(self, title):
-        return self.select(Query("SELECT * FROM wikipoints WHERE wp_title='{}'".format(title)))
+        return self.select(Query(u"SELECT * FROM wikipoints WHERE wp_title='{}'".format(title)))
 
     def selectByIds(self, ids):
         ids = '(' + ','.join(map(str, ids)) + ')'
-        return self.select(Query("SELECT * FROM wikipoints WHERE wp_id IN {}".format(ids)))
+        return self.select(Query(u"SELECT * FROM wikipoints WHERE wp_id IN {}".format(ids)))
 
     def selectTitles(self):
-        return self.select(Query("SELECT wp_title FROM wikipoints"))
+        return self.select(Query(u"SELECT wp_title FROM wikipoints"))
 
 class WikimapCategoriesTable(TableProxy):
     def __init__(self, tablePath):
         super(WikimapCategoriesTable, self).__init__(tablePath, useCustomTypes=True)
 
     def create(self):
-        self.execute(Query("""
+        self.execute(Query(u"""
             CREATE TABLE wikicategories (
                 wc_title        TEXT        NOT NULL,
                 wc_page_ids     LIST        NOT NULL
             );"""))
 
     def populate(self, values):
-        self.executemany(Query("INSERT INTO wikicategories VALUES (?,?)", "populating wikicategories table", logStart=True), values)
-        self.execute(Query("CREATE UNIQUE INDEX title_idx ON wikicategories(wc_title);", "creating index title_idx in wikicategories table", logStart=True, logProgress=True))
+        self.executemany(Query(u"INSERT INTO wikicategories VALUES (?,?)", "populating wikicategories table", logStart=True), values)
+        self.execute(Query(u"CREATE UNIQUE INDEX title_idx ON wikicategories(wc_title);", "creating index title_idx in wikicategories table", logStart=True, logProgress=True))
 
     def selectByTitle(self, title):
-        return self.select(Query("SELECT * FROM wikicategories WHERE wc_title='{}'".format(title)))
+        return self.select(Query(u"SELECT * FROM wikicategories WHERE wc_title='{}'".format(title)))
 
     def selectTitles(self):
-        return self.select(Query("SELECT wc_title FROM wikicategories"))
+        return self.select(Query(u"SELECT wc_title FROM wikicategories"))
