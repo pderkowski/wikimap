b370dfdf5889511c87d4a7b31f5b47710d39171e
Piotr Derkowski <piotder@gmail.com>
2017-02-18 17:07:42 +0100

    Faster nn queries.

--- wikimap/NearestNeighbors.py
+++ wikimap/NearestNeighbors.py
@@ -1,18 +1,19 @@
-from sklearn.neighbors import NearestNeighbors
 import logging
-import Utils
+import vptree
 
 def computeNearestNeighbors(data, neighborsNo):
     logger = logging.getLogger(__name__)
 
     logger.info('Loading data.')
-    data = Utils.any2array(data)
+    data = list(data)
 
-    logger.info('Creating ball tree.')
-    nbrs = NearestNeighbors(n_neighbors=neighborsNo+1, algorithm='ball_tree', metric='euclidean').fit(data) # +1, because the closest point is the point itself
+    logger.info('Creating vptree tree.')
+
+    tree = vptree.VpTree(data)
 
     logger.info('Computing neighbors and distances.')
-    distances, indices = nbrs.kneighbors(data)
-
-    return distances[:, 1:], indices[:, 1:] # for every point its closest neighbor is the point itself, return only real neighbors
+    batch = tree.getNearestNeighborsBatch(data, neighborsNo+1)
+    distances = [dists[1:] for dists, _ in batch] # for every point its closest neighbor is the point itself, return only real neighbors
+    indices = [inds[1:] for _, inds in batch] # for every point its closest neighbor is the point itself, return only real neighbors
+    return distances, indices
 
